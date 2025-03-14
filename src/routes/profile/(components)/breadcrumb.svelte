0738b81a51c6c1a1f5c0e4f0254ed54908651039
faizftw <faizfatwa030206@gmail.com>
2025-03-14 13:41:44 +0800

    feat: tambahkan komponen breadcrumb di halaman profil
    - Impor dan tambahkan komponen BreadCrumb untuk navigasi yang lebih baik
    - Perbarui layout halaman profil dengan meningkatkan lebar kontainer

---
+++ src/routes/profile/(components)/breadcrumb.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+    import {Home } from "lucide-svelte"
+</script>
+
+<Breadcrumb.Root>
+    <Breadcrumb.List>
+  <Breadcrumb.Item>
+    <Breadcrumb.Link href="/" class="flex items-center">
+        <Home class="h-4 w-4 mr-1" />
+        <span>Home</span>
+    </Breadcrumb.Link>
+  </Breadcrumb.Item>
+  <Breadcrumb.Separator  class="flex items-center" />
+  <Breadcrumb.Item>
+    <Breadcrumb.Page  class="flex items-center">Profile</Breadcrumb.Page>
+  </Breadcrumb.Item>
+</Breadcrumb.List>
+</Breadcrumb.Root>
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,9 +1,14 @@
 <script>
   import UserProfile from "./(components)/user-profile.svelte";
+  import BreadCrumb from "./(components)/breadcrumb.svelte"
 </script>
 
 <main class="min-h-screen py-8 px-4 sm:px-6 lg:px-8">
-  <div class="max-w-4xl mx-auto">
+  <div class="max-w-7xl mx-auto">
+    <div class="mb-3">
+      <BreadCrumb/>
+    </div>
+    
     <UserProfile />
   </div>
 </main> 
